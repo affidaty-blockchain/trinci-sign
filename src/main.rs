e697ffce6fdab7861891c90d5ac27526c74672e9
Daniele Forti <d.forti@affidaty.io>
2022-04-01 15:09:41 +0200

    added create_unit_tx functionality

---
+++ src/main.rs
@@ -0,0 +1,163 @@
+use clap::{Arg, Command};
+use serde_value::value;
+use trinci_core::{
+    base::{
+        schema::{SignedTransaction, TransactionData},
+        serialize::rmp_serialize,
+    },
+    crypto::{
+        ecdsa::{CurveId, KeyPair as EcdsaKeyPair},
+        Hash,
+    },
+    KeyPair, Message, TransactionDataV1,
+};
+use types::{AppCommand, Arguments, Result, UnitTxArgs};
+
+mod types;
+
+fn get_args() -> Option<AppCommand> {
+    let matches = Command::new("Trinci Blockchain Transaction Sign")
+        .version(clap::crate_version!())
+        .author(clap::crate_authors!())
+        .about(clap::crate_description!())
+        .arg(
+            Arg::new("command")
+                .long("command")
+                .short('c')
+                .help("Specify the command: { create_unit_tx }")
+                .value_name("COMMAND")
+                .required(true),
+        )
+        .arg(
+            Arg::new("hex")
+                .long("hex")
+                .short('h')
+                .help("Arguments in messagepacked HEX")
+                .value_name("HEX")
+                .required_unless_present("json")
+                .required_unless_present("bs58"),
+        )
+        .arg(
+            Arg::new("json")
+                .long("json")
+                .short('j')
+                .help("Arguments in json String")
+                .value_name("JSON")
+                .required_unless_present("hex")
+                .required_unless_present("bs58"),
+        )
+        .arg(
+            Arg::new("bs58")
+                .long("bs58")
+                .short('b')
+                .help("Arguments in messagepacked base58")
+                .value_name("BASE58")
+                .required_unless_present("json")
+                .required_unless_present("hex"),
+        )
+        .get_matches();
+
+    let operation = matches.value_of("command").unwrap_or_default();
+
+    let args = match operation {
+        "create_unit_tx" => {
+            let inner_args = if let Some(hex_text) = matches.value_of("hex") {
+                UnitTxArgs::from_hex_string(hex_text)
+            } else if let Some(json_text) = matches.value_of("json") {
+                UnitTxArgs::from_json_string(json_text)
+            } else if let Some(bs58_text) = matches.value_of("bs58") {
+                UnitTxArgs::from_bs58_string(bs58_text)
+            } else {
+                None
+            };
+            match inner_args {
+                Some(args) => Arguments::UnitTxArgsType(args),
+                None => return None,
+            }
+        }
+        _ => return None,
+    };
+
+    Some(AppCommand {
+        operation: operation.to_string(),
+        args,
+    })
+}
+
+/// Convert a base58 string into a vec
+fn bs58_into_vec(bs58_text: &str) -> Result<Vec<u8>> {
+    bs58::decode(bs58_text).into_vec().map_err(|e| e.into())
+}
+
+fn create_unit_tx(input_args: Arguments) -> Result<()> {
+    match input_args {
+        Arguments::UnitTxArgsType(input_args) => {
+            let contract = if input_args.contract.is_empty() {
+                None
+            } else {
+                Hash::new(
+                    trinci_core::crypto::HashAlgorithm::Sha256,
+                    &bs58_into_vec(&input_args.contract)?,
+                )
+                .ok()
+            };
+
+            let private_bytes = bs58_into_vec(&input_args.private_key)?;
+            let public_bytes = bs58_into_vec(&input_args.public_key)?;
+
+            let kp = EcdsaKeyPair::new(CurveId::Secp384R1, &private_bytes, &public_bytes)?;
+
+            let data_args = value!(input_args.args);
+            let args = rmp_serialize(&data_args)?;
+
+            let nonce = bs58_into_vec(&input_args.nonce)?;
+
+            let data = TransactionData::V1(TransactionDataV1 {
+                account: input_args.target,
+                fuel_limit: input_args.fuel,
+                nonce,
+                network: input_args.network,
+                contract,
+                method: input_args.method,
+                caller: trinci_core::PublicKey::Ecdsa(kp.public_key()),
+                args,
+            });
+
+            // TODO
+            let signature = data.sign(&KeyPair::Ecdsa(kp))?;
+
+            let sign_tx = SignedTransaction { data, signature };
+
+            let tx = trinci_core::Transaction::UnitTransaction(sign_tx);
+
+            let message = Message::PutTransactionRequest { confirm: true, tx };
+
+            // TODO
+            // Message pack of the transaction and save as .bin
+            let buf = rmp_serialize(&message)?;
+
+            std::fs::write("transaction.bin", buf).unwrap();
+
+            println!("Trinci Transaction saved into `transaction.bin`");
+            Ok(())
+        }
+    }
+}
+
+fn main() {
+    let args = get_args();
+    match args {
+        Some(val) => match val.operation.as_str() {
+            "create_unit_tx" => {
+                create_unit_tx(val.args).expect("Error creating unit tx message");
+            }
+            _ => {
+                println!("Command error!")
+            }
+        },
+        None => println!("Arguments error!"),
+    }
+}
+
+#[cfg(test)]
+mod tests {}
---
+++ src/types.rs
@@ -0,0 +1,104 @@
+use serde::{Deserialize, Serialize};
+
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+pub struct AppCommand {
+    pub operation: String,
+    pub args: Arguments,
+}
+
+pub enum Arguments {
+    UnitTxArgsType(UnitTxArgs),
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct UnitTxArgs {
+    pub target: String,
+    pub network: String,
+    pub nonce: String, // base58 of a bytes array
+    pub fuel: u64,
+    pub contract: String,
+    pub method: String,
+    pub args: String,
+    pub public_key: String,  // base58 of a bytes array
+    pub private_key: String, // base58 of a bytes array
+}
+
+impl UnitTxArgs {
+    pub fn from_json_string(json_text: &str) -> Option<Self> {
+        match serde_json::from_str::<UnitTxArgs>(json_text) {
+            Ok(val) => Some(val),
+            Err(_) => None,
+        }
+    }
+
+    pub fn from_hex_string(hex_text: &str) -> Option<Self> {
+        match hex::decode(hex_text) {
+            Ok(buf) => match rmp_serde::from_slice::<UnitTxArgs>(&buf) {
+                Ok(val) => Some(val),
+                Err(_) => None,
+            },
+            Err(_) => None,
+        }
+    }
+
+    pub fn from_bs58_string(bs58_text: &str) -> Option<Self> {
+        match bs58::decode(bs58_text).into_vec() {
+            Ok(buf) => match rmp_serde::from_slice::<UnitTxArgs>(&buf) {
+                Ok(val) => Some(val),
+                Err(_) => None,
+            },
+            Err(_) => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    const ARGS_HEX: &str = "DF00000009A6746172676574A8234143434F554E54A76E6574776F726BA6534B594E4554A56E6F6E6365AB343363394A475973715971A46675656CCD2710A8636F6E7472616374D9443132323035616330636666313839653232373934623834373638373537386566343731346337646131306665396536663665333133363439323836333631623038323766A66D6574686F64AE6D795F636F6F6C5F6D6574686F64A461726773B57B2761273A3132332C2762273A2768656C6C6F277DAA7075626C69635F6B6579D92C32376D3556674A59324A5062775548356E50627139644E705251597539506459327146514150343279727A35AB707269766174655F6B6579D957754C376B3473704B413276686658314E6464373470555739554E7A44476D4362584868725A6B354C734C414B6366566F6B356762723667596F53374671436766525738666B483559664A34645941436B72743848543668";
+    const ARGS_JSON: &str = "{\"target\":\"#ACCOUNT\",\"network\":\"SKYNET\",\"nonce\":\"43c9JGYsqYq\",\"fuel\":10000,\"contract\":\"12205ac0cff189e22794b847687578ef4714c7da10fe9e6f6e313649286361b0827f\",\"method\":\"my_cool_method\",\"args\":\"{'a':123,'b':'hello'}\",\"public_key\":\"27m5VgJY2JPbwUH5nPbq9dNpRQYu9PdY2qFQAP42yrz5\",\"private_key\":\"uL7k4spKA2vhfX1Ndd74pUW9UNzDGmCbXHhrZk5LsLAKcfVok5gbr6gYoS7FqCgfRW8fkH5YfJ4dYACkrt8HT6h\"}";
+    const ARGS_BS58: &str = "3PuEMd5N4u3L6WzutXHrpYEtXFPu2gmoLKQudpt2jzrHhb9o2K5FNp5qDVK1mV35bKUhbbR4Uy12FtxjHGJLEe4uwTnuGGmyTtXuBs5TWXGgrvC87h286CNpz5FWrzVjZVPMJNb71aFhWRqT4AxBG9yiX6Lvz7WvwR6RunhzieAtMpLdGgvWuMWCoaCrUDCN28QLPLFKgPkcB6D24qDMxM1MPz5bFzAP3U5N2hoSmEduaBh8dFrFUZpARpZ4txF9it5khmwrkPEirwPDw4kpvF5GhqdM2Rxn3NfRnomzDGocxHftgf4Z77SSe3xb8APmtDWw5vWT7orKKTVoCs4TjYMQRAwz4Hh7L9TVdGoVyhG1UifCKZ4TT95Wuf6YaMon8SmyS6ULFyDbX1YCrRefgTLVGkAsReUeGhST1Atv9ntn64swnHpVeNgytycW5nJ4kCsf5kNaLbW5qYsdZ7trQUPZd5Q3";
+
+    fn create_unit_tx_args() -> UnitTxArgs {
+        UnitTxArgs {
+            target: String::from("#ACCOUNT"),
+            network: String::from("SKYNET"),
+            nonce: String::from("43c9JGYsqYq"),
+            fuel: 10000u64,
+            contract: String::from("12205ac0cff189e22794b847687578ef4714c7da10fe9e6f6e313649286361b0827f"),
+            method: String::from("my_cool_method"),
+            args: String::from("{'a':123,'b':'hello'}"),
+            public_key: String::from("27m5VgJY2JPbwUH5nPbq9dNpRQYu9PdY2qFQAP42yrz5"),
+            private_key: String::from("uL7k4spKA2vhfX1Ndd74pUW9UNzDGmCbXHhrZk5LsLAKcfVok5gbr6gYoS7FqCgfRW8fkH5YfJ4dYACkrt8HT6h"),
+        }
+    }
+
+    #[test]
+    fn unit_tx_args_from_hex() {
+        let expected = create_unit_tx_args();
+
+        let res = UnitTxArgs::from_hex_string(ARGS_HEX).unwrap();
+
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn unit_tx_args_from_json() {
+        let expected = create_unit_tx_args();
+
+        let res = UnitTxArgs::from_json_string(ARGS_JSON).unwrap();
+
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn unit_tx_args_from_bs58() {
+        let expected = create_unit_tx_args();
+
+        let res = UnitTxArgs::from_bs58_string(ARGS_BS58).unwrap();
+
+        assert_eq!(res, expected);
+    }
+}
